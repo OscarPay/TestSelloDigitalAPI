6aea7cff514f3aa894827c254fc894a0447a3bde
oscar <nike-o_94@hotmail.com>
2016-06-29 11:09:12 -0500

    Se agrego un parametro para probar en dos cuentas diferentes las facturas

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,18 +2,18 @@ class InvoicesController < ApplicationController
   require 'rest-client'
 
   HOST = 'http://api.localhost.com:3000'.freeze
-  ACCESS_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjoyLCJuYW1lIjoiVmljdG9yIiwiZW52aXJvbWVudCI6ImRldmVsb3BtZW50IiwidGltZSI6MTQ2NzEyODA0NH19.0oNLVkUYhAT8g_21d8Ss7Nju-auZew8-9BqlxLMpAi12_GWzSJhZLyl8-sGmlTtM5iFAak07Oa2RLBEcrvXhyw'.freeze
+  ACCESS_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjoyLCJuYW1lIjoiVmljdG9yIiwiZW52aXJvbWVudCI6ImRldmVsb3BtZW50IiwidGltZSI6MTQ2NzIxMjk2NH19.jGG_zMc-MqBrbBQh9WpWv9kA2vA9ciVJ75tDCt71tWAXUV_-6BMo5dTqH-08frDCIGBxgymqRE3FdhyK2E05Qw'.freeze
+  ACCESS_TOKEN2 = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozLCJuYW1lIjoiSm9yZ2UiLCJlbnZpcm9tZW50IjoiZGV2ZWxvcG1lbnQiLCJ0aW1lIjoxNDY3MjE0NDc2fX0.hB3pGD7RWUceJtmJdEmGaz6oe0zhAat9HF1C6D5j-3qFFz6R1Gg4L4Yj6fx0eCVGNjGslWy9wEoTbCTMRyW3Yg'.freeze
 
   def index
     path = Rails.root.join("public", "invoices")
     Dir.mkdir(path) unless File.exist?(path)
 
-    response = RestClient.get(HOST + '/invoices/1', {Authorization: ACCESS_TOKEN})
+    response = RestClient.get(HOST + '/invoices/9', {Authorization: ACCESS_TOKEN2})
     json_response = JSON.parse(response.body)
 
     @status = json_response["invoice_status"]
     @invoice = json_response
-
 
     if @status === "Timbrada"
       xml_doc = Nokogiri::XML(json_response["xml"])
@@ -71,7 +71,7 @@ class InvoicesController < ApplicationController
                               note: 'asdasd', discount_amount: 50, issuer_rfc: 'AAD990814BP7',
                               payment_method_id: 1, receipt_type_id: 1, money_id: 1,
                               is_test: 0, invoice_type_id: 1, discount_type_id: 1,
-                              branch_attributes: [name: "Sucursal prueba4456", phone: "9252525", serie: "V", folio: "1",
+                              branch_attributes: [name: "Sucursal prueba4456", phone: "9252525", serie: "V", folio: "25",
                                                   address_attributes: address_invoice],
                               receptor_attributes: [name: "Pruebas456", social_reason: "Prueba Reason", rfc: "PEAO031212A8A",
                                                     email: "test@hotmail.com", address_attributes: address_invoice],
